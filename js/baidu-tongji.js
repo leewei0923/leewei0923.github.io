81eb74cfd37178c8ea75e76bb52c2403fe0d166c
leewei0923 <leewei0923@github.com>
2020-07-05 00:16:34 +0800

    Site updated: 2020-07-05 00:16:34

---
+++ js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?728e2f4b4e09573f785084760c3a8b8f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
